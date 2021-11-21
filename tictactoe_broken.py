b363810acfc6f7ace0eabc7e5aadd7bebca68520
haribkhalid <93860657+haribkhalid@users.noreply.github.com>
2021-11-21 13:57:59 +0400

    third change

--- tictactoe_broken.py
+++ tictactoe_broken.py
@@ -67,7 +67,7 @@ def add_piece(game, player, row, column):
     row: 0-index_row
     column: 0-index_column
     
-    game[row][column+1] = player
+    game[row][column] = player
     return game
 
 def check_space_empty(game, row, column):
